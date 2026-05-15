b51f2e661321e4bec2982a6e241e6c5526f8f2db
Tim Schneeberger <tim.schneeberger@outlook.de>
2026-05-15 19:10:43 +0200

    Support format strings for logger

--- src/utils/logger.cpp
+++ src/utils/logger.cpp
@@ -2,8 +2,10 @@
 
 #include <chrono>
 #include <cstdio>
+#include <cstdarg>
 #include <iomanip>
 #include <sstream>
+#include <vector>
 
 #include "nxlink_sink.h"
 
@@ -21,6 +23,30 @@ void Logger::set_sink(Sink sink) {
 void Logger::shutdown() {
     sink_ = nullptr;
     nxlink_.shutdown();
+}
+
+std::string Logger::format(const char* fmt, ...) {
+    if (!fmt) {
+        return {};
+    }
+
+    va_list args;
+    va_start(args, fmt);
+    va_list args_copy;
+    va_copy(args_copy, args);
+    const int size = std::vsnprintf(nullptr, 0, fmt, args_copy);
+    va_end(args_copy);
+
+    if (size <= 0) {
+        va_end(args);
+        return {};
+    }
+
+    std::vector<char> buffer(static_cast<size_t>(size) + 1);
+    std::vsnprintf(buffer.data(), buffer.size(), fmt, args);
+    va_end(args);
+
+    return std::string(buffer.data(), static_cast<size_t>(size));
 }
 
 namespace {
--- src/utils/logger.h
+++ src/utils/logger.h
@@ -2,6 +2,8 @@
 
 #include <functional>
 #include <string>
+#include <type_traits>
+#include <utility>
 
 #include "nxlink_sink.h"
 
@@ -19,7 +21,29 @@ public:
     static void error(const std::string& msg);
     static void log(const std::string& level, const std::string& msg);
 
+    template <typename... Args, typename = std::enable_if_t<(sizeof...(Args) > 0)>>
+    static void info(const char* fmt, Args&&... args) {
+        log("I", format(fmt, std::forward<Args>(args)...));
+    }
+
+    template <typename... Args, typename = std::enable_if_t<(sizeof...(Args) > 0)>>
+    static void warn(const char* fmt, Args&&... args) {
+        log("W", format(fmt, std::forward<Args>(args)...));
+    }
+
+    template <typename... Args, typename = std::enable_if_t<(sizeof...(Args) > 0)>>
+    static void error(const char* fmt, Args&&... args) {
+        log("E", format(fmt, std::forward<Args>(args)...));
+    }
+
+    template <typename... Args, typename = std::enable_if_t<(sizeof...(Args) > 0)>>
+    static void log(const std::string& level, const char* fmt, Args&&... args) {
+        log(level, format(fmt, std::forward<Args>(args)...));
+    }
+
 private:
     static Sink sink_;
     static NxLink nxlink_;
+
+    static std::string format(const char* fmt, ...);
 };
