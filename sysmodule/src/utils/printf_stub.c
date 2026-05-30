416355058b6581022b378516fd83bae5875930a4
Tim Schneeberger <tim.schneeberger@outlook.de>
2026-05-30 21:20:12 +0200

    Add printf library and wrap snprintf/vsnprintf for lightweight usage

---
+++ sysmodule/src/utils/printf_stub.c
@@ -0,0 +1,23 @@
+#include <printf/printf.h>
+#include <stdarg.h>
+#include <stddef.h>
+
+/**
+ * Use lightweight printf implementations to avoid increasing the binary size greatly.
+ *
+ * Currently we only use:
+ *  - snprintf: in
+ *  - vsnprintf: in Logger
+ */
+
+int __wrap_snprintf(char *s, size_t n, const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    int ret = vsnprintf_(s, n, fmt, args);
+    va_end(args);
+    return ret;
+}
+
+int __wrap_vsnprintf(char *s, size_t n, const char *fmt, va_list args) {
+    return vsnprintf_(s, n, fmt, args);
+}
